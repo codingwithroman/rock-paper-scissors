67a441c38e7643096bf4856ef1e31ee10aa38b45
codingwithroman <r.knoef@hotmail.com>
2024-09-17 16:39:24 +0200

    complete program to play one game of rock, paper , scissors
    add function that gets the computer choice through picking a random option from an array with the math.random method
    add function that will get user choice through the prompt method which input gets checked on validity using the .included method
    add function that will play the game for one round and adding logic to deal with every different outcome of the game and add a point to the winning side's score

--- my-script.js
+++ my-script.js
@@ -1,1 +1,68 @@
-console.log("Hello World!")
+const gameOptions = [
+    "rock", 
+    "paper", 
+    "scissors"
+];
+
+function getComputerChoice() {
+
+    const randomOptionIndex = Math.floor(Math.random() * gameOptions.length);
+    
+    return gameOptions[randomOptionIndex];
+
+}
+
+
+function getHumanChoice() {
+  
+    const choice = prompt("Choose rock, paper or scissors:").toLowerCase();
+
+    if (gameOptions.includes(choice)) {
+        return choice;
+    } else {
+        alert("Invalid. Please refresh the page and try again.");
+        return null;
+    }
+}
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+function playRound(humanChoice, computerChoice){
+
+    if (!humanChoice) {
+        return "No game due too invalid answer";
+    }
+
+    if (humanChoice === computerChoice) {
+        return "It's a tie!";
+    }
+
+    if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+     ) {
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}`;
+    } else {
+        computerScore++;
+        return `You lose! ${computerChoice} beats ${humanChoice}`;
+    }
+}
+
+
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+const result = playRound(humanSelection, computerSelection)
+
+
+console.log(`Your choice: ${humanSelection}
+Computer choice: ${computerSelection}`)
+console.log(result)
+console.log(`Your score: ${humanScore}
+Computer score: ${computerScore}`)
